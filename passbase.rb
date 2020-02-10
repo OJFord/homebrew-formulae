fbaf47921d49d66e38cfb357510ea71223358e13
Ollie Ford <dev@ojford.com>
2020-02-10 18:26:33 +0000

    Bump passbase to v0.1.1

--- passbase.rb
+++ passbase.rb
@@ -1,11 +1,12 @@
 class Passbase < Formula
   desc "Keybase File System (KBFS)-baskced CLI-based password manager"
   homepage "https://github.com/OJFord/passbase"
-  url "https://github.com/OJFord/passbase/releases/download/v0.1/passbase"
-  version "0.1"
-  sha256 "6dced5b07090c4c916d4df6535acba6f805764d2cc9feb12e2b690b4b3f4e0cc"
+  url "https://github.com/OJFord/passbase/releases/download/v0.1.1/passbase-darwin"
+  version "0.1.1"
+  sha256 "c07ff22a29ec4e554c450c2a073183d824af39681dec8201e4ddbcad6b52a0d6"
 
   def install
+    system "mv passbase-darwin passbase"
     bin.install "passbase"
   end
 
