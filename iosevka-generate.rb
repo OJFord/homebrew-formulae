6ac921e585966ffc809daae69df7eb12dcd0191e
Oliver Ford <dev@ojford.com>
2024-04-05 17:10:42 -0700

    Fix dependencies for modern iosevka versions

--- iosevka-generate.rb
+++ iosevka-generate.rb
@@ -3,9 +3,9 @@ class IosevkaGenerate < Formula
   homepage "https://github.com/OJFord/iosevka-generate"
   head "https://github.com/OJFord/iosevka-generate.git", :branch => "master"
 
+  depends_on "fontforge"
   depends_on "python"
   depends_on "node"
-  depends_on "caryll/tap/otfcc-mac64"
   depends_on "ttfautohint"
 
   resource "GitPython" do
@@ -18,19 +18,24 @@ class IosevkaGenerate < Formula
     sha256 "a3ebbc27be035a2e874ed904df516e35f4a29a778a764385de09de9e0f139658"
   end
 
+  resource "inflection" do
+    url "https://files.pythonhosted.org/packages/e8/dc/3986343faf9631d8bc61d8a6a1331b5f4f08723dbce3b39f524c367a1621/inflection-0.4.0.tar.gz"
+    sha256 "32a5c3341d9583ec319548b9015b7fbdf8c429cbcb575d326c33ae3a0e90d52c"
+  end
+
   resource "pyxdg" do
     url "https://files.pythonhosted.org/packages/26/28/ee953bd2c030ae5a9e9a0ff68e5912bd90ee50ae766871151cd2572ca570/pyxdg-0.25.tar.gz"
     sha256 "81e883e0b9517d624e8b0499eb267b82a815c0b7146d5269f364988ae031279d"
   end
 
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/9d/be/10918a2eac4ae9f02f6cfe6414b7a155ccd8f7f9d4380d62fd5b955065c3/requests-2.31.0.tar.gz"
+    sha256 "942c5a758f98d790eaed1a29cb6eefc7ffb0d1cf7af05c3d2791656dbd6ad1e1"
+  end
+
   resource "smmap" do
     url "https://files.pythonhosted.org/packages/bc/aa/b744b3761fff1b10579df996a2d2e87f124ae07b8336e37edc89cc502f86/smmap-0.9.0.tar.gz"
     sha256 "0e2b62b497bd5f0afebc002eda4d90df9d209c30ef257e8673c90a6b5c119d62"
-  end
-
-  resource "inflection" do
-    url "https://files.pythonhosted.org/packages/e8/dc/3986343faf9631d8bc61d8a6a1331b5f4f08723dbce3b39f524c367a1621/inflection-0.4.0.tar.gz"
-    sha256 "32a5c3341d9583ec319548b9015b7fbdf8c429cbcb575d326c33ae3a0e90d52c"
   end
 
   include Language::Python::Virtualenv
